9530ea01db9c30445219fa5c14bb21b105616c07
Alex <alexvkleschov@gmail.com>
2022-10-15 23:17:19 +0400

    rewrite classes

--- head_first_book/mixins/duck.py
+++ head_first_book/mixins/duck.py
@@ -1,10 +1,7 @@
-from abc import ABC
-
-class Duck:
-    pass
+from abc import ABC, abstractmethod
 
 
-class FlyBehavior(ABS):
+class FlyBehavior(ABC):
 
     @abstractmethod
     def fly(self):
@@ -21,7 +18,7 @@ class FlyNoWay(FlyBehavior):
         print("Cannot fly!")
 
 
-class QuackBehavior(ABS):
+class QuackBehavior(ABC):
 
     @abstractmethod
     def quack(self):
@@ -42,3 +39,27 @@ class MuteQuack(QuackBehavior):
     def quack(self):
         print("Silence")
 
+class Duck:
+    """Общий класс для уток"""
+    def __init__(self, fly_behavior: FlyBehavior, quack_behavior: QuackBehavior):
+        self.fly_behavior = fly_behavior
+        self.quack_behavior = quack_behavior
+
+    def swim(self):
+        print("Утка плывет")
+
+    def perform_quack(self):  # Делегируем поведение объекту-компоненту
+        self.quack_behavior.quack()
+
+    def perform_fly(self):
+        self.fly_behavior.fly()
+
+    @abstractmethod
+    def display(self):
+        pass
+
+
+mullard_duck = Duck(FlyWithWings(), Squeak())
+
+mullard_duck.perform_fly()
+mullard_duck.perform_quack()
