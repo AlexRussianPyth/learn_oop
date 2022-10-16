735ccc36fb286bd6d3cc2f8726c0e4dd2dbd20b0
Alex <alexvkleschov@gmail.com>
2022-10-16 21:17:09 +0400

    add factory uml

---
+++ head_first_book/factory/pizza_factory.py
@@ -0,0 +1,31 @@
+from abc import ABC
+
+
+class SimplePizzaFactory:
+    def create_pizza(pizza_type: str):
+        pizza = None
+        if pizza_type == 'veggie':
+            pizza = Veggie()
+        elif pizza_type == 'meat':
+            pizza = MeatPizza()
+        return pizza
+
+
+class PizzaStore(ABC):
+    def __init__(self, pizza_factory):
+        self.pizza_factory = pizza_factory
+
+    def order_pizza(self, pizza_type: str):
+        """Метод, позволяющий заказать конкретную пиццу"""
+        pizza = self.create_pizza(pizza_type)
+
+        pizza.prepare()
+        pizza.bake()
+        pizza.cut()
+        pizza.box()
+
+        return pizza
+
+    @abstractmethod
+    def create_pizza(self, pizza_type):
+        pass
