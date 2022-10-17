a24060c10ae1ecf4cd2f74ae1c0bc62afa466530
Alex <alexvkleschov@gmail.com>
2022-10-17 23:05:12 +0400

    add other realisation

--- head_first_book/singleton/singleton.py
+++ head_first_book/singleton/singleton.py
@@ -4,3 +4,18 @@ class Singleton:
             cls.instance = super(Singleton, cls).__new__(cls)
         return cls.instance
 
+
+# Other Realisation
+class Logger(object):
+    _instance = None
+
+    def __init__(self):
+        raise RuntimeError('Call instance() instead')
+
+    @classmethod
+    def instance(cls):
+        if cls._instance is None:
+            print('Creating new instance')
+            cls._instance = cls.__new__(cls)
+            # Put any initialization here.
+        return cls._instance
