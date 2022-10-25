c123d7565194d6764b1b864df5369b5165238709
Alex <alexvkleschov@gmail.com>
2022-10-26 00:46:24 +0400

    add remotecontrol test

--- head_first_book/command/simple.py
+++ head_first_book/command/simple.py
@@ -2,6 +2,7 @@ import abc
 
 
 class Command(abc.ABC):
+    """Класс-интерфейс для паттерна Команда"""
     @abc.abstractmethod
     def execute(self):
         pass
@@ -14,3 +15,31 @@ class LightOnCommand(Command):
     def execute(self):
         self.light.on()
 
+class Light:
+    def on(self):
+        print("Свет включается")
+
+
+class SimpleRemoteControl:
+    """Простой пульт с одним слотом для подключения устройств и одной кнопкой"""
+    def set_command(self, command: Command):
+        """Загрузить команду. Можно вызвать повторно, если захотим изменить команду"""
+        self.slot = command
+
+    def button_was_pressed(self):
+        """Выполняем команду"""
+        self.slot.execute()
+
+
+class RemoteControlTest:
+    def __init__(self):
+        self.remote = SimpleRemoteControl()
+        self.light = Light()
+        self.light_on = LightOnCommand(self.light)
+
+
+        self.remote.set_command(self.light_on)
+        self.remote.button_was_pressed()
+
+
+foo = RemoteControlTest()
