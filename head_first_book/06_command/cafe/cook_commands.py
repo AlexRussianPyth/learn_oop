f16116e190f215cd8d4d4ce9f7edb6cabfd66c26
Alex <alexvkleschov@gmail.com>
2022-11-13 22:03:17 +0400

    add uml

--- head_first_book/06_command/cafe/cook_commands.py
+++ head_first_book/06_command/cafe/cook_commands.py
@@ -1,5 +1,5 @@
 from command import Command
-from cooks import SushiCook
+from cooks import SushiCook, Shaurmist
 
 
 class SushiCookMakeSushiOrder(Command):
@@ -9,3 +9,12 @@ class SushiCookMakeSushiOrder(Command):
     def execute(self):
         self.cook.grind_knifes()
         self.cook.make_roll()
+
+
+class ShaurmistMakeShaurmaOrder(Command):
+    def __init__(self, cook: Shaurmist):
+        self.cook = cook
+
+    def execute(self):
+        self.cook.smoke()
+        self.cook.make_shaurma()
