b789101ba26dd7fed15d96b488591d6b7a9a269e
Alex <alexvkleschov@gmail.com>
2022-11-27 11:29:46 +0400

    fix decorator folder

--- 03_decorator/beverages/main.py
+++ 03_decorator/beverages/main.py
@@ -1,13 +1,15 @@
 from beverage import Espresso, DarkRoast, Mocha, Soy
 
 
-# Закажем обычный "пустой" кофе
-beverage = Espresso()
-print(beverage.cost())
+if __name__ == "__main__":
+    # Закажем обычный "пустой" кофе
+    beverage = Espresso()
+    print(beverage.cost())
 
-# Создаем Черный кофе, добавляем 2 Мокки и Сою
-beverage2 = DarkRoast()
-beverage2 = Mocha(beverage2)
-beverage2 = Mocha(beverage2)
-beverage2 = Soy(beverage2)
-print(f"My coffee: {beverage2.get_description()} costs: {beverage2.cost()}$")
+    # Создаем Черный кофе, добавляем 2 Мокки и Сою
+    beverage2 = DarkRoast()
+    beverage2 = Mocha(beverage2)
+    beverage2 = Mocha(beverage2)
+    beverage2 = Soy(beverage2)
+    print(f"""My coffee: {beverage2.get_description()}
+    costs: {beverage2.cost()}$""")
